b09ece4413234cd89245608f856e1cad42b32104
Kaelan Kaiser <kaisergw2@gmail.com>
2024-06-12 18:16:53 -0700

    Set up the Flask server and webhook to accept and respond to texts

--- main.py
+++ main.py
@@ -2,3 +2,24 @@ import json
 from flask import Flask, request, jsonify
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=["GET", "POST"])
+def command():
+
+    try:
+        body = request.form.get("Body")
+        
+        resp = MessagingResponse()
+        resp.message(body)
+        return str(resp)
+    
+    except Exception as e:
+        resp = MessagingResponse()
+        resp.message("An error occured, please try again!")
+
+        return str(resp)
+    
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
